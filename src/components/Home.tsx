44b89404bb833a6fcd1e950d1cb9a50a8072a317
TeamT1deS <team_t1des@outlook.com>
2024-10-25 16:32:23 +0800

    Initial commit. 2024/10/25

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Activity, ArrowRight, Plus } from "lucide-react";
+import { Activity, ArrowRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import "@/assets/css/Home.css";
 
